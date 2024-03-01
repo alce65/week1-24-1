f99a11c41124484db6295c4c484a5685f30f36b8
alce65 <alce65@hotmail.es>
2024-03-01 16:00:58 +0100

    Add meta

---
+++ meta.ts
@@ -0,0 +1,12 @@
+type Meta = {
+  title?: string;
+  description?: string;
+  keywords?: string[];
+  image?: string;
+  url?: string;
+};
+
+const meta: Meta = {
+  title: 'InSitu Week 1',
+  description: 'InSitu Week 1',
+};
